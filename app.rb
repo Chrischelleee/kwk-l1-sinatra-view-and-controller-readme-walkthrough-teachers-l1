a164948e2ab64722e5bfde1a7aa635c86a59495b
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-06-26 17:33:48 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,16 +1,26 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello World"
+  end
   get '/reverse' do
     erb :reverse
   end
 
   post '/reverse' do
+    original_string = params["string"]
+    @reversed_string = original_string.reverse
     erb :reversed
   end
 
   get '/friends' do
     # Write your code here!
+    post '/reverse' do
+    puts params
 
+    erb :reversed
+      end
+      erb :friends
   end
 end
